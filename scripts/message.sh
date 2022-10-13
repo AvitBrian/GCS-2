fb30ddd49f81675c48faf1134ef79e1820accdb4
AvitBrian <b.mugisha2@alustudent.com>
2022-10-13 15:19:23 -0700

    Added Long to negpod9.txt

--- scripts/message.sh
+++ scripts/message.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #custom messages
+
 for name in $(cat negpod9.txt)
 do
 	
 	for Recipeints_Name in template.txt
 	do
-		sed "s|Recipeints_Name|$name|" template.txt > $name.txt
+		sed "s|Recipeints_Name|$name|" template.txt > $(pwd)/messages/$name.txt	
+		
 	done
 done
